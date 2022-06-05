5d8956fd0e8119e9f1252a8c21055bee9500daae
Vladisl-Sh <106905364+Vladisl-Sh@users.noreply.github.com>
2022-06-05 11:43:38 +0300

    Add files via upload

---
+++ 7.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2_Dispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Resource> obj = new List<Resource>();
+            obj.Add(new Resource(1));
+            obj.Add(new Resource(2));
+            obj.Add(new Resource(3));
+            
+
+            foreach (Resource o in obj)
+            {
+                o.Disposing(o);
+            }
+
+            Console.WriteLine("Deleting completed!");
+            Console.ReadKey();
+        }
+
+    }
+   
+}
+
---
+++ 7.2/Recources.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _7._2_Dispose
+{
+    class Resource
+    {
+        public int b { get; set; }
+
+        private bool disp = false;
+
+        public Resource(int b)
+        {
+            this.b = b;
+        }
+
+        public void Disposing(Resource obj)
+        {
+            StreamWriter sw = new StreamWriter(@"D:\!!! УНИВЕР\4 сем\Новая папка\7.2", true);
+            if (!disp)
+            {
+                sw.WriteLine(DateTime.UtcNow.ToString());
+                sw.WriteLine("Object {0} is removed.", obj.b);
+                sw.WriteLine();
+                obj = null;
+                GC.SuppressFinalize(this);
+                disp = true;
+            }
+            sw.Close();
+        }
+
+
+    }
+}
